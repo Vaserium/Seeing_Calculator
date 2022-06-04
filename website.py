a3e982d13090d9f2c79864302e14165e0d07aed3
Toby Avila <88242860+Vaserium@users.noreply.github.com>
2022-06-04 11:30:50 -0400

    Add files via upload

--- website.py
+++ website.py
@@ -17,8 +17,7 @@ with row3_1:
         "'Why do you want to measure the seeing value?' Well, this interactive application containing FWHM(Full Width Half Maximum) data allows you to look at measured seeing conditions so you can stay informed on the night each observation was taken!")
     st.markdown("Seeing: “Good seeing” is when the stars that are being displayed on your image twinkle very little; “bad seeing” is when they twinkle a lot. Check the diagram below...")
     st.markdown("Pickering Scale: A scale from 1 to 10 which represents seeing conditions.  A rating of 1 is very poor seeing, and a rating of 10 is perfect seeing.")
-    st.markdown("How to check seeing conditions: https://www.skyatnightmagazine.com/advice/what-is-astronomical-seeing/")
-
+    st.markdown("[How to check seeing conditions](https://www.skyatnightmagazine.com/advice/what-is-astronomical-seeing/)")
 st.write(data)
 
 a = alt.Chart(dataToDisplay).mark_area(opacity=1).encode(
