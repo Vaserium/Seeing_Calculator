bb5f2de909df4cbe62f1972f9e891ae799b2dff4
Toby Avila <88242860+Vaserium@users.noreply.github.com>
2022-06-08 10:45:36 -0400

    Add files via upload

--- website.py
+++ website.py
@@ -8,16 +8,45 @@ st.header('York Observatory Seeing Network')
 # ------------------------------------------------- Displaying all data (just for show and will not actually use data from this)
 excel_file = 'sample.csv'
 
+latext = r'''
+$$ 
+halfMax = \frac{min(data) + max(data)}{2} \\
+FWHM = i_1 - i_2 + 1 \\
+$$
+    where i1 is where the data first drops below halfMax, i2 is where the data last rises above halfMax.  
+'''
+
 data = pd.read_csv(excel_file)
 dataToDisplay = pd.DataFrame(data)
 
 row3_1, row3_spacer2 = st.columns((3.2, .1))
 with row3_1:
+    st.subheader("Project Introduction")
     st.markdown(
-        "'Why do you want to measure the seeing value?' Well, this interactive application containing FWHM(Full Width Half Maximum) data allows you to look at measured seeing conditions so you can stay informed on the night each observation was taken!")
-    st.markdown("Seeing: “Good seeing” is when the stars that are being displayed on your image twinkle very little; “bad seeing” is when they twinkle a lot. Check the diagram below...")
-    st.markdown("Pickering Scale: A scale from 1 to 10 which represents seeing conditions.  A rating of 1 is very poor seeing, and a rating of 10 is perfect seeing.")
-    st.markdown("[How to check seeing conditions](https://www.skyatnightmagazine.com/advice/what-is-astronomical-seeing/)")
+        "This website contains my 2022 co-op investigation into how seeing conditions affect day-to-day observations at Allan I. Carswell Astronomical Observatory. One of the measurements I was interested in was the seeing value. 'Why do you want to measure the seeing value?' Well, this interactive application containing FWHM(Full Width Half Maximum) data allows you to look at measured seeing conditions so you can stay informed on the night each observation was taken!")
+    st.markdown(
+        "How are seeing and FWHM related? I used the number of pixels across the width of the star at half its maximum value, ")
+    st.write(latext)
+    st.markdown(
+        "For this investigation I used the Pickering scale which is [a scale from 1 to 10 which represents seeing conditions. A rating of 1 is very poor seeing, and a rating of 10 is perfect seeing](https://freestarcharts.com/pickering-scale#:~:text=Pickering%20scale%20The%20scale%20developed%20by%20Harvard%20astronomer,and%20a%20rating%20of%2010%20meaning%20perfect%20seeing.). "
+        "Here are some handy definitions for seeing: “Good seeing” is when the stars that are being displayed on your image twinkle very little; “bad seeing” is when they twinkle a lot. "
+        "Check the diagram below to see what this might look like in the Pickerings scale.")
+
+column1, column2 = st.columns((1.5, 1))
+image = Image.open('pickering.png')
+st.image(image, caption='Credit: Damian Peach (http://www.damianpeach.com/pickering.htm)', width=620)
+
+st.subheader("Project Data")
+st.markdown("For my project I measured the FWHM of stars in an image field from [you include description of object observed here]. "
+            "These data were taken at the Allan I Carswell Observatory 1m telescope. "
+            "All 2022 data I took with members of the observatory for this project and previous data was used from the archive. "
+            "Here is a picture of our star field for [you include description here]")
+
+st.markdown("ADD STAR IMAGE HERE")
+
+st.markdown("The question I wanted to answer with my project is what kind of seeing do we get at the Allan I Carswell observatory, "
+            "see below for the data table and a plot showing all my values. As a reminder, the conversion for the Pickering scale was done with [you include description here].")
+
 st.write(data)
 
 a = alt.Chart(dataToDisplay).mark_area(opacity=1).encode(
@@ -26,14 +55,3 @@ a = alt.Chart(dataToDisplay).mark_area(opacity=1).encode(
 c = alt.layer(a)
 
 st.altair_chart(c, use_container_width=True)
-
-column1, column2 = st.columns((1.5, 1))
-image = Image.open('pickering.png')
-st.image(image, caption='Credit: Damian Peach', width=620)
-
-
-
-
-
-
-
