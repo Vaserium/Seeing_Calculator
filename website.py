4fe7c0b1526b68532d11a68b75e0a4d0aaae6ad7
Toby Avila <88242860+Vaserium@users.noreply.github.com>
2022-06-17 22:41:30 -0400

    Add files via upload

--- website.py
+++ website.py
@@ -22,13 +22,11 @@ st.markdown(
 
 g1, g2 = st.columns((1, 2.5))
 
-image1 = Image.open('pickering2.png')
-g2.image1(image1,
-         caption='Credit: Kolář J (https://eaae-astronomy.org/images/projects/catch-a-star/2015/18_How_to_measure_seeing.pdf)',
-         width=450)
+image1 = Image.open('Qatar1.png')
+g1.image(image1, caption='Credit: Kolář J (https://eaae-astronomy.org/images/projects/catch-a-star/2015/18_How_to_measure_seeing.pdf)',width=450)
 
 image2 = Image.open('pickering2.png')
-g2.image2(image2,
+g2.image(image2,
          caption='Credit: Kolář J (https://eaae-astronomy.org/images/projects/catch-a-star/2015/18_How_to_measure_seeing.pdf)',
          width=620)
 
